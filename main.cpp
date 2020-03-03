85e6c26fa984992832cf07b06f0a33e511d4328c
CasualCoder91 <alarich2@hotmail.com>
2020-03-04 00:51:41 +0100

    a bit of cleanup

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 #include <random>
 #include <vector>
 #include <chrono> //for timer
-#include <string.h>
 
 #include "Star.h"
 #include "Node.h"
@@ -11,10 +10,7 @@
 #include "Integrator.h"
 #include "InitialConditions.h"
 #include "Analysis.h"
-//#include "Window.h"
-//#include "ShaderProgram.h"
 
-using namespace std::chrono;
 
 int main() {
 
@@ -23,63 +19,26 @@ int main() {
 //		std::cout<<"test\n";
 //	}
 	//init stars
-	int n_Stars = 100;
-	double boxLength = 1; //[]?
+	int n_Stars = 2;
+	double boxLength = 1; //[pc]
 	double dt = 1;
-	int NTimesteps = 100000;
-	steady_clock::time_point startTime = steady_clock::now();
-	//Window window = Window(n_Stars,boxLength*10);
-	//window.createWindow();
-	//window.initOpenGL();
+	int NTimesteps = 10;
+	std::chrono::steady_clock::time_point startTime = std::chrono::steady_clock::now();
 
 	std::random_device rd;  //Will be used to obtain a seed for the random number engine
 	std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
 	std::uniform_real_distribution<> dis(0.0, boxLength);
 	std::vector<Star*> stars = {};
+
 	//Init
 	double totalMass = InitialConditions::InitialMass(stars,n_Stars);
 	InitialConditions::PlummerSphere(stars, 1, totalMass);
 	//stars.push_back(new Star(1000, 0, 0, 0));
 	//totalMass += 1000;
-	//window.initBuffers(stars);
-	//glfwSetTime(0);
-	//float current_time;
-	//float last_time = glfwGetTime();
-	//float dt1;
-	//glEnable(GL_BLEND);
-	//ShaderProgram shader;
-	//std::string vertexShader = "vertexShader.txt";
-	//char* cstr = new char[vertexShader.length() + 1];
-	//strcpy(cstr, vertexShader.c_str());
-	//std::string fragmentShader = "fragmentShader.txt";
-	//char* cstr2 = new char[fragmentShader.length() + 1];
-	//strcpy(cstr2, fragmentShader.c_str());
-	//std::string ProjectionMatrix = "ProjectionMatrix";
-	//char* cstr3 = new char[ProjectionMatrix.length() + 1];
-	//strcpy(cstr3, ProjectionMatrix.c_str());
-	//window.projectionMatrix.createProjectionMatrix(70.0f, 400.0f / 400.0f, 0.1f, 100.0f);
-	//shader.loadMat4(cstr3, &window.projectionMatrix);
 
-	int frameMod = 0;
-	//for (int i = 0; i < n_Stars; i++) {
-	//	stars.push_back( new Star(1, dis(gen), dis(gen), dis(gen)));
-	//}
 	//Integrate
 	Integrator euler = Integrator(dt);
 	for (int i = 0; i < NTimesteps; i++) {
-
-		//SYSTEMTIME start;
-		//GetSystemTime(&start);
-
-		//current_time = glfwGetTime();
-		//dt1 = current_time - last_time;
-		//last_time = current_time;
-
-
-		//if (window.keys[GLFW_KEY_T]) {
-		//	window.keys[GLFW_KEY_T] = false;
-		//	window.afterImages = !window.afterImages;
-		//}
 
 		Vec3D tlf = Vec3D(), brb = Vec3D();
 		Node::FindCorners(tlf, brb, stars);
@@ -93,22 +52,7 @@ int main() {
 			stars.at(i)->acceleration = Vec3D(); // reset acceleration to 0,0,0
 			root.ApplyForce(stars.at(i));
 		}
-		//for (Star* star : stars) {
-		//	star->acceleration = Vec3D(); // reset acceleration to 0,0,0
-		//	root.ApplyForce(star);
-		//}
 		euler.Euler(stars);
-
-		//window.Input(dt1);
-		//window.UpdateBuffers(stars);
-		//window.camera.update();
-		//shader.use();
-		//glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
-		//glBlendFunc(GL_ONE, GL_ZERO);
-		//glBindVertexArray(window.VAO);
-		//glDrawArraysInstanced(GL_POINTS, 0, 1, n_Stars);
-		//glfwPollEvents();
-		//glfwSwapBuffers(window.window);
 
 		if (i % 100 == 0) {
 			InOut::WriteWithLabel(stars, "./Output/stars" + std::to_string(i) + ".dat");
@@ -120,11 +64,11 @@ int main() {
 			//std::cout << "Total Energy: " + std::to_string(kineticEnergy+potentialEnergy) << std::endl << std::endl;
 		}
 	}
-	steady_clock::time_point endTime = steady_clock::now();
-	duration<double> time_span = duration_cast<duration<double>>(startTime - endTime);
+	std::chrono::steady_clock::time_point endTime = std::chrono::steady_clock::now();
+	std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>>(endTime - startTime);
 	//InOut::Write(stars,"stars.dat");
 	//InOut::Write(&root);
-	std::cout << "Time needed" << time_span.count();
+	std::cout << "Time needed: " << time_span.count() << "seconds" << std::endl;
 	std::cout << "done" << std::endl;
 	std::cin.get();
 	return 0;
