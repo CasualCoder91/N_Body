7a28f34ba558b57655c2802f9fbc027cfdd22358
CasualGamer <alarich123@gmail.com>
2020-05-14 16:40:58 +0200

    )/&%(/&%

--- docs/conf.py
+++ docs/conf.py
@@ -85,8 +85,8 @@ read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 breathe_projects = {}
 
 if read_the_docs_build:
-    input_dir = '..'
-    output_dir = '_build'
+    input_dir = '../N_Body'
+    output_dir = 'build'
     configureDoxyfile(input_dir, output_dir)
     subprocess.call('doxygen', shell=True)
     breathe_projects["N_Body"] = output_dir + '/xml'
