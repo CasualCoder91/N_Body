a2ffe272e33cded93a72401df146d1604ea25e11
CasualGamer <alarich123@gmail.com>
2020-10-24 00:33:07 +0200

    mass instead of timestep

--- Misc/fileFromDB.py
+++ Misc/fileFromDB.py
@@ -18,7 +18,7 @@ def createConnection(db_file):
 
 def select2dPositions(conn, simulationID):
     cur = conn.cursor()
-    cur.execute("""SELECT position.timestep,position2D.x,position2D.y FROM star
+    cur.execute("""SELECT position.timestep,position2D.x,position2D.y,star.mass FROM star
        INNER JOIN position on position.id_star = star.id
        INNER JOIN position2D on position.id=position2D.fk_position
        where star.id_simulation = ?1 order by position.timestep""", (simulationID,)) #and star.isCluster=0 LIMIT 10000000 OFFSET 10000000 and star.isCluster=1
@@ -27,7 +27,7 @@ def select2dPositions(conn, simulationID):
 
 def main():
     simulationID = 1
-    database = r"E:\Master_Thesis\VS_Project\N_Body\Output\Database\default.db"
+    database = r"E:\Master_Thesis\VS_Project\N_Body\Output\Database\NGC2244.db"
     output = r"E:\Master_Thesis\VS_Project\N_Body\Output\NGC2244"# + str(simulationID)
     # create a database connection
     conn = createConnection(database)
@@ -38,7 +38,7 @@ def main():
             #plt.scatter(timestepData[:,1], timestepData[:,2])
             #plt.show()
             if(i<9 or (i%4==0 and i<52) or (i%13==0 and i>52)):
-                np.savetxt(output+r"\NGC2244_pos_"+str(i)+'.dat', timestepData, delimiter=',')   # X is an array
+                np.savetxt(output+r"\NGC2244_pos_"+str(int(i))+'.dat', timestepData[:,1:], delimiter=',')   # X is an array
 
 if __name__ == '__main__':
     main()
