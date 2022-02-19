23b332a28292f3f82e11264a934b53f86b26e64f
CasualGamer <alarich2@hotmail.com>
2022-02-19 19:11:08 +0100

    rework: Fig "stars sampled from the Salpeter IMF", untill Galactic potential

--- docs/_build/latex/pyplots/initialConditionsMassSalpeter.py
+++ docs/_build/latex/pyplots/initialConditionsMassSalpeter.py
@@ -11,17 +11,17 @@ def initialConditionsMassSalpeter(dataPath='',showPlot=True,arguments=[]):
     fig = plt.figure()
 
     # histogram on linear scale
-    plt.subplots_adjust(hspace = 0.4)
-    plt.subplot(211)
-    plt.title('10e3 Stars sampled from Salpeter IMF')
-    plt.xlabel('mass [$M_{\odot}$]')
-    plt.ylabel('Count')
-    hist, bins, _ = plt.hist(x, bins=200)
+    # plt.subplots_adjust(hspace = 0.4)
+    # plt.subplot(211)
+    # plt.title('10e3 Stars sampled from Salpeter IMF')
+    # plt.xlabel('mass [$M_{\odot}$]')
+    # plt.ylabel('Count')
+    # hist, bins, _ = plt.hist(x, bins=200)
 
     # histogram on log scale.
     # Use non-equal bin sizes, such that they look equal on log scale.
     logbins = np.logspace(np.log10(bins[0]),np.log10(bins[-1]),len(bins))
-    plt.subplot(212)
+    # plt.subplot(212)
     plt.hist(x, bins=logbins)
     plt.xscale('log')
     plt.yscale('log')
--- docs/pyplots/initialConditionsMassSalpeter.py
+++ docs/pyplots/initialConditionsMassSalpeter.py
@@ -8,26 +8,27 @@ def initialConditionsMassSalpeter(dataPath='',showPlot=True,arguments=[]):
         n = int(arguments[0])
     data = np.loadtxt(dataPath+'initialConditionsMassSalpeter'+str(n)+'.dat',delimiter=',')
     x = data[:,1]
-    fig = plt.figure()
-
+    fig = plt.figure(figsize=(8, 3))
+    ax = plt.gca()
     # histogram on linear scale
-    plt.subplots_adjust(hspace = 0.4)
-    plt.subplot(211)
+    #plt.subplots_adjust(hspace = 0.4)
+    #plt.subplot(211)
     #plt.title('10e3 Stars sampled from Salpeter IMF')
     plt.xlabel('mass [$M_{\odot}$]')
     plt.ylabel('Count')
-    hist, bins, _ = plt.hist(x, bins=200)
-    plt.ticklabel_format(axis='y',style='sci', scilimits=(0,0))
-    
+    #hist, bins, _ = plt.hist(x, bins=400)
+
     # histogram on log scale.
     # Use non-equal bin sizes, such that they look equal on log scale.
-    logbins = np.logspace(np.log10(bins[0]),np.log10(bins[-1]),len(bins))
-    plt.subplot(212)
+    logbins = np.logspace(np.log10(x[0]),np.log10(np.amax(x)),400)
+    #plt.subplot(212)
     plt.hist(x, bins=logbins)
     plt.xscale('log')
+    #ax.set_xticks([25, 50, 75])
     plt.yscale('log')
     plt.xlabel('mass [$M_{\odot}$]')
     plt.ylabel('Count')
+    plt.tight_layout()
     if showPlot:
         plt.show()
 
