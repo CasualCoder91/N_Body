8744ac7c1e7a37ffac35a404a577d36518bda525
CasualGamer <alarich2@hotmail.com>
2022-02-20 13:33:18 +0100

    thesis. Summary of part 1. More description for figures

--- docs/pyplots/initialConditionsMassBulge.py
+++ docs/pyplots/initialConditionsMassBulge.py
@@ -5,26 +5,27 @@ from mpl_toolkits.mplot3d import Axes3D
 def initialConditionsMassBulge(dataPath='', showPlot=True, count = 10000):
     data = np.loadtxt(dataPath+'initialConditionsMassBulge'+str(count)+'.dat',delimiter=',')
     x = data[:,1]
-    fig = plt.figure()
+    fig = plt.figure(figsize=(8, 3))
 
     # histogram on linear scale
-    plt.subplots_adjust(hspace = 0.4)
-    plt.subplot(211)
+    # plt.subplots_adjust(hspace = 0.4)
+    # plt.subplot(211)
     #plt.title('10e3 Stars sampled from Bulge MF')
     plt.xlabel('mass [$M_{\odot}$]')
     plt.ylabel('Count')
-    hist, bins, _ = plt.hist(x, bins=200)
-    plt.ticklabel_format(axis='y',style='sci', scilimits=(0,0))
+    # hist, bins, _ = plt.hist(x, bins=200)
+    # plt.ticklabel_format(axis='y',style='sci', scilimits=(0,0))
 
     # histogram on log scale.
     # Use non-equal bin sizes, such that they look equal on log scale.
-    logbins = np.logspace(np.log10(bins[0]),np.log10(bins[-1]),len(bins))
-    plt.subplot(212)
+    logbins = np.logspace(np.log10(np.amin(x)),np.log10(np.amax(x)),400)
+    # plt.subplot(212)
     plt.hist(x, bins=logbins)
     plt.xscale('log')
     plt.yscale('log')
     plt.xlabel('mass [$M_{\odot}$]')
     plt.ylabel('Count')
+    plt.tight_layout()
     if showPlot:
         plt.show()
 
--- docs/pyplots/initialConditionsMassDisk.py
+++ docs/pyplots/initialConditionsMassDisk.py
@@ -9,26 +9,27 @@ def initialConditionsMassDisk(dataPath='', showPlot=True, arguments=[]):
 
     data = np.loadtxt('initialConditionsMassDisk'+str(count)+'.dat',delimiter=',')
     x = data[:,1]
-    fig = plt.figure()
+    fig = plt.figure(figsize=(8, 3))
 
     # histogram on linear scale
-    plt.subplots_adjust(hspace = 0.4)
-    plt.subplot(211)
+    #plt.subplots_adjust(hspace = 0.4)
+    #plt.subplot(211)
     #plt.title('Stars with total mass of 10e3 $M_{\odot}$ sampled from disk PDMF')
     plt.xlabel('mass [$M_{\odot}$]')
     plt.ylabel('Count')
-    hist, bins, _ = plt.hist(x, bins=200)
-    plt.ticklabel_format(axis='y',style='sci', scilimits=(0,0))
+    #hist, bins, _ = plt.hist(x, bins=200)
+    #plt.ticklabel_format(axis='y',style='sci', scilimits=(0,0))
 
     # histogram on log scale.
     # Use non-equal bin sizes, such that they look equal on log scale.
-    logbins = np.logspace(np.log10(bins[0]),np.log10(bins[-1]),len(bins))
-    plt.subplot(212)
+    logbins = np.logspace(np.log10(np.amin(x)),np.log10(np.amax(x)),400)
+    #plt.subplot(212)
     plt.hist(x, bins=logbins)
     plt.xscale('log')
     plt.yscale('log')
     plt.xlabel('mass [$M_{\odot}$]')
     plt.ylabel('Count')
+    plt.tight_layout()
     if showPlot:
         plt.show()
 
